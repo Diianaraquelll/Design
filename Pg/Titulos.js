9f77cb2536bfb0e5ce28c9e9d07cf84c44e8506e
Diianaraquelll <57532577+Diianaraquelll@users.noreply.github.com>
2019-12-04 11:36:03 -0800

    .

---
+++ Pg/Titulos.js
@@ -0,0 +1,11 @@
+
+	var xpto = document.getElementById(
+	//df = 5;
+	df = "Loja de Batatas";
+	xpto.innerHTML = df;
+
+	var xpto = document.getElementById(
+	//df = 5;
+	df = "Brasil no seu melhor";
+	xpto.innerHTML = df1;
+	
